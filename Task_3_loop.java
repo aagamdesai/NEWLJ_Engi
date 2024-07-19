37cb95fa6aeba44566c97e3017f66c17c304bde3
aagamdesai <112149685+aagamdesai@users.noreply.github.com>
2024-07-19 09:44:24 +0530

    Update Task_3_loop.java

--- Task_3_loop.java
+++ Task_3_loop.java
@@ -1,9 +1,11 @@
 public class Task_3_loop{
     public static void main(String[] args) {
-        int i=1;
-        do { 
-            System.out.println(i);
+        int i=0;
+        while(i<=10){ 
+            if(i%2!=0) {
+                System.out.println(i);
+            }
             i++;
-        } while (i<=5);
+        }
     }
 }
